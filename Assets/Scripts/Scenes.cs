f5af61447d9e3c05caba1bd4a1c5802021f7a4cd
Vikramad98 <gaikwadvikramaditya@gmail.com>
2020-09-21 19:51:24 +0530

    commit 3

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Scenes : MonoBehaviour
+{
+    
+
+    public void scene1()
+    {
+        SceneManager.LoadScene("1");
+    }
+    public void scene2()
+    {
+        SceneManager.LoadScene("2");
+    }
+    public void scene3()
+    {
+        SceneManager.LoadScene("3");
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
